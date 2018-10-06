3f0ef0b619110d8c50df7f1c200ade17ac18c8a6
Joshua Castor <jcasto3@uic.edu>
2018-10-06 11:39:04 -0500

    Added initial vuetify template, created initial main app bar component

--- frontend-site/src/App.vue
+++ frontend-site/src/App.vue
@@ -1,29 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      v-model="showNavDrawer"
+      enable-resize-watcher
+      fixed app>
+      the nav drawer
+      <!-- <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list> -->
+    </v-navigation-drawer>
+    <main-app-bar @sidemenu-button-click="showNavDrawer = !showNavDrawer"/>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+import MainAppBar from '@/components/MainAppBar';
+
+export default {
+  name: 'App',
+  components: {
+    MainAppBar,
+  },
+  data () {
+    return {
+      showNavDrawer: true,
+    };
+  },
+};
+</script>
---
+++ frontend-site/src/components/MainAppBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-toolbar
+    app :clipped-left="false">
+    <v-toolbar-side-icon @click="$emit('sidemenu-button-click')"/>
+    <v-toolbar-title v-text="title"/>
+    <v-spacer/>
+    <v-btn flat outline>
+      <v-icon>person_add</v-icon>
+      <span>Sign Up</span>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>fab fa-slack</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
