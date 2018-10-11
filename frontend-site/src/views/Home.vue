a5adb6d2a7e3a23fd44c98f99586e849dd9390a6
Joshua Castor <jcasto3@uic.edu>
2018-10-11 18:02:51 -0500

    moved SVG code into its own component for easy styling, added filler section for about and location

--- frontend-site/src/views/Home.vue
+++ frontend-site/src/views/Home.vue
@@ -1,9 +1,25 @@
 <template>
-  <v-container fluid>
+  <v-container grid-list-md>
     <v-layout justify-center>
-      <object type="image/svg+xml" :data="require('@/assets/LUG_logo_Non-UIC.svg')" class="full-logo">
-        LUG Logo
-      </object>
+      <lug-banner-svg class="full-logo mb-4"/>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 md8 xl9>
+        <h1 class="title">About</h1>
+        <p>
+          The LUG description goes here.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Consectetur libero id faucibus nisl tincidunt eget nullam non. Velit aliquet sagittis id consectetur purus ut faucibus.
+          Feugiat pretium nibh ipsum consequat nisl vel pretium.
+          </p>
+      </v-flex>
+      <v-flex xs12 sm6 md4 xl3>
+        <h1 class="title">Location</h1>
+        <p>
+          The LUG location goes here.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </v-flex>
     </v-layout>
     <v-layout column align-center>
       <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
@@ -13,10 +29,19 @@
   </v-container>
 </template>
 
+<script>
+import LugBannerSvg from '@/components/LugBannerSvg';
+
+export default {
+  components: {
+    LugBannerSvg,
+  },
+};
+</script>
+
+
 <style lang="scss">
 .full-logo {
-  path {
-    fill: green!important;
-  }
+  max-width: 100%;
 }
 </style>
