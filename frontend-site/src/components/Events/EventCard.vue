55003e666d4279cc2d19f4f4f9e75c1c866429f4
Joshua Castor <jcasto3@uic.edu>
2018-10-30 13:47:34 -0500

    created events card component

---
+++ frontend-site/src/components/Events/EventCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-card :dark="!useLightTheme" :light="useLightTheme" raised class="event-card">
+    <v-container fluid class="pl-5 pr-5">
+      <v-layout>
+        <v-flex>
+          <h1 class="headline">{{ event.summary }}</h1>
+          <h2 class="subheading">{{ dateHeading }}</h2>
+          <h2 class="subheading">{{ locationDescription }}</h2>
+        </v-flex>
+      </v-layout>
+      <v-layout>
+        <v-flex>
+          {{ eventDescription }}
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  props: {
+    event: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    ...mapState(['useLightTheme']),
+    isSameDate () {
+      const startDate = new Date(this.event.timeStart);
+      const endDate = new Date(this.event.timeEnd);
+      return [
+        startDate.getYear() === endDate.getYear(),
+        startDate.getMonth() === endDate.getMonth(),
+        startDate.getDate() === endDate.getDate(),
+      ].every(isTrue => isTrue);
+    },
+    dateHeading () {
+      if (this.isSameDate) {
+        const startDate = new Date(this.event.timeStart);
+        const endDate = new Date(this.event.timeEnd);
+        return `${startDate.toLocaleDateString()}: ${startDate.toLocaleTimeString()} to ${endDate.toLocaleTimeString()}`;
+      } else {
+        return [
+          new Date(this.event.timeStart).toLocaleString(),
+          new Date(this.event.timeEnd).toLocaleString(),
+        ].join(' to ');
+      }
+    },
+    locationDescription () {
+      return `Location: ${this.event.location || 'None Specified'}`;
+    },
+    eventDescription () {
+      return this.event.description || 'No description specified';
+    },
+  },
+};
+</script>
+
+<style>
+.event-card {
+  height: 100%;
+}
+</style>
--- frontend-site/src/components/Home/EventCarousel.vue
+++ frontend-site/src/components/Home/EventCarousel.vue
@@ -1,27 +1,18 @@
 <template>
   <v-carousel class="event-carousel" v-model="activeEvent" hide-delimiters>
     <v-carousel-item v-for="(event, i) in events" :key="i">
-      <!-- TODO: move to separate component? -->
-      <v-card
-        :dark="!useLightTheme"
-        :light="useLightTheme"
-        raised
-        class="event-carousel-card">
-        <v-card-title>
-          {{ event.summary }}<br>
-          {{ new Date(event.timeStart).toLocaleString() }} to {{ new Date(event.timeEnd).toLocaleString() }}
-        </v-card-title>
-        <v-card-text>
-          {{ event }}
-        </v-card-text>
-      </v-card>
+      <event-card :event="event"/>
     </v-carousel-item>
   </v-carousel>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
+import EventCard from '@/components/Events/EventCard';
 export default {
+  components: {
+    EventCard,
+  },
   computed: {
     ...mapState(['useLightTheme']),
     ...mapState('events', {
@@ -47,8 +38,5 @@ export default {
 .event-carousel {
   // TODO: determine a good size, or do programmatically?
   height: 300px;
-  .event-carousel-card {
-    height: 100%;
-  }
 }
 </style>
