c9ac74dabf42cb462908d5c4e32369af4892208b
Joshua Castor <jcasto3@uic.edu>
2018-11-06 11:37:08 -0600

    Added who we are and constitution link

--- frontend-site/src/views/About.vue
+++ frontend-site/src/views/About.vue
@@ -7,10 +7,17 @@
             <h1 class="title">Who We Are</h1>
           </v-card-title>
           <v-card-text>
-            who we are goes here.
+            The Linux Users Group at University of Illinois at Chicago (LUG@UIC) is a student organization whose purpose is to provide a community for students that are interested in all things related to Linux and Open Source Technology.
+            As a community we strive to provide the support and education that students seek.
+            Through various events and activities we work towards building a bigger community and spreading the use of Linux and open source software.
           </v-card-text>
           <v-card-actions>
-            <v-btn flat>Constitution</v-btn>
+            <v-btn
+            href="https://github.com/lugatuic/lug-docs/blob/master/rfc0000-lug-constitution.md"
+            flat target="_blank"
+            rel="noopener">
+              Constitution
+            </v-btn>
           </v-card-actions>
         </v-card>
       </v-flex>
