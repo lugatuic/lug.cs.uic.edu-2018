973eb6ad250c684643379d0cf2fe26aa31ed6f78
Joshua Castor <jcasto3@uic.edu>
2018-11-06 10:13:51 -0600

    Update mock data to use new officer structure, default images to tux

--- frontend-site/src/components/OfficerCard.vue
+++ frontend-site/src/components/OfficerCard.vue
@@ -1,7 +1,9 @@
 <template>
   <v-card class="officer-card">
-    <v-img :src="officer.image" aspect-ratio = '2.75'></v-img>
-    
+    <v-img
+      contain
+      :src="avatarUrl"
+      aspect-ratio="2.75"/>
     <v-card-title>
       <h1 class="title">{{ officer.name }}</h1>
     </v-card-title>
@@ -19,24 +21,29 @@ export default {
       required: true,
     },
   },
+  computed: {
+    avatarUrl () {
+      return this.officer.image || require('@/assets/whiteTux.png');
+    },
+  },
 };
 </script>
 
-<div class="col-md-3" id="StephenWalden">
-    <div class="card card-01">
-      <div class="profile-box">
-          <img class="card-img-top rounded-circle" alt="Card image cap" src="./avatar.jpg">
-      </div>
-      <div class="card-body text-center">
-        <h4 class="card-title">Stephen Walden</h4>
-        <p class="card-text">I started coding with computer related video games. I have been developing simple command line interfaces since the age of 14, and have been working on learning about technology ever since that time. Lately I have been working on several different items. You can see a sample of what I have been working on if you visit my website.</p>
-        <span class="social-box">
-            <a id="fb" href="https://www.facebook.com/stephen.walden.100" target="_blank"><i class="fa fa-facebook"></i></a>
-            <a class="hidden" id="tw" href="#" target="_blank"><i class="fa fa-twitter"></i></a>
-            <a class="hidden" id="gp" href="#" target="_blank"><i class="fa fa-google-plus"></i></a>
-            <a id="li" href="https://www.linkedin.com/in/stephen-3-walden" target="_blank"><i class="fa fa-linkedin"></i></a>
-            <a id="lk" href="https://walden1995.github.io/" target="_blank"><i class="fa fa-external-link-square"></i></a>
-        </span>
-      </div>
-    </div>
-  </div>
+// <div class="col-md-3" id="StephenWalden">
+//     <div class="card card-01">
+//       <div class="profile-box">
+//           <img class="card-img-top rounded-circle" alt="Card image cap" src="./avatar.jpg">
+//       </div>
+//       <div class="card-body text-center">
+//         <h4 class="card-title">Stephen Walden</h4>
+//         <p class="card-text">I started coding with computer related video games. I have been developing simple command line interfaces since the age of 14, and have been working on learning about technology ever since that time. Lately I have been working on several different items. You can see a sample of what I have been working on if you visit my website.</p>
+//         <span class="social-box">
+//             <a id="fb" href="https://www.facebook.com/stephen.walden.100" target="_blank"><i class="fa fa-facebook"></i></a>
+//             <a class="hidden" id="tw" href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+//             <a class="hidden" id="gp" href="#" target="_blank"><i class="fa fa-google-plus"></i></a>
+//             <a id="li" href="https://www.linkedin.com/in/stephen-3-walden" target="_blank"><i class="fa fa-linkedin"></i></a>
+//             <a id="lk" href="https://walden1995.github.io/" target="_blank"><i class="fa fa-external-link-square"></i></a>
+//         </span>
+//       </div>
+//     </div>
+//   </div>
