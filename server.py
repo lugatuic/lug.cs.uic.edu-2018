7cf0ebe7a720d73071aa5882d5eb96ae9fb66d21
BreadYogurt <brentagainagain@gmail.com>
2018-11-12 12:45:52 -0600

    Implemented event caching.

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from icalendar import Calendar
 import requests #using this to yank the ics file from google calendar
+from datetime import datetime, timedelta
 
 class Positions(Enum):
   """
@@ -231,16 +232,25 @@ def getOfficers():
   # Convert Officer objects back to dictionaries before returning
   return jsonify([x.__dict__ for x in officers])
 
+#I'm keeping these global variables here for now so it's easier to determine that it goes with the events api when the pieces of the api are inevitably split up
+ev = []
+eventsCached = datetime.now()
 @app.route('/api/events')
 def getEvents():
   calurl = 'https://calendar.google.com/calendar/ical/ca149os3pmnh0dcopr1jn2negg%40group.calendar.google.com/public/basic.ics'
   dtstrformat = '%Y-%m-%dT%H:%M:%S.%fZ'
-  gcal = Calendar.from_ical(requests.get(calurl).content)
-  ev = []
-  for comp in [x for x in gcal.walk() if x.name=="VEVENT"]:
-     ev.append({'summary': comp.decoded('SUMMARY').decode('UTF-8'), 
-      'timeStart': comp.decoded('DTSTART').strftime(dtstrformat),
-      'timeEnd': comp.decoded('DTEND').strftime(dtstrformat),
-      'location': comp.decoded('LOCATION').decode('UTF-8'),
-      'description': comp.decoded('DESCRIPTION').decode('UTF-8')})
+  global ev
+  global eventsCached
+
+  if (ev == []) or (datetime.now() - eventsCached >= timedelta(hours=1)):
+    gcal = Calendar.from_ical(requests.get(calurl).content)
+    ev = []
+    for comp in [x for x in gcal.walk() if x.name=="VEVENT"]:
+      ev.append({'summary': comp.decoded('SUMMARY').decode('UTF-8'), 
+        'timeStart': comp.decoded('DTSTART').strftime(dtstrformat),
+        'timeEnd': comp.decoded('DTEND').strftime(dtstrformat),
+        'location': comp.decoded('LOCATION').decode('UTF-8'),
+        'description': comp.decoded('DESCRIPTION').decode('UTF-8')})
+    eventsCached = datetime.now()
+    
   return jsonify(ev)
