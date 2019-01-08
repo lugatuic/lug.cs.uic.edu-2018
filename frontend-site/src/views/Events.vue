4b501761bbd19bf9b07cfcee8ea9bf308dc390e4
Joshua Castor <jcasto3@uic.edu>
2019-01-07 21:18:46 -0600

    Added initial logic for loading events on events page

--- frontend-site/src/views/Events.vue
+++ frontend-site/src/views/Events.vue
@@ -1,3 +1,26 @@
 <template>
-  <h1>This is the events page</h1>
+  <h1>This is the events page. {{ events }}</h1>
 </template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+import EventCard from '@/components/Home/EventCard';
+
+export default {
+  components: {
+    EventCard,
+  },
+  computed: {
+    ...mapState(['useLightTheme']),
+    ...mapState('events', {
+      events: 'data',
+    }),
+  },
+  methods: {
+    ...mapActions('events', ['updateData']),
+  },
+  async mounted () {
+    await this.updateData();
+  },
+};
+</script>
