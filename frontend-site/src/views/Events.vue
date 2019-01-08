c2f603acd0cdc329b0a94ab004cce634ce101f07
Joshua Castor <jcasto3@uic.edu>
2019-01-07 21:41:48 -0600

    Added initial event listing

--- frontend-site/src/views/Events.vue
+++ frontend-site/src/views/Events.vue
@@ -1,5 +1,29 @@
 <template>
-  <h1>This is the events page. {{ events }}</h1>
+  <v-container grid-list-lg>
+    <v-layout row>
+      <v-flex>
+        Search Card here
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex class="text-xs-center" v-if="events.length === 0">
+        No events found.
+      </v-flex>
+      <template v-else>
+        <v-flex
+          v-for="(event, i) in viewableEvents"
+          :key="i"
+          xs12 md6>
+          <event-card :event="event"/>
+        </v-flex>
+      </template>
+    </v-layout>
+    <v-layout row>
+      <v-flex class="text-xs-center">
+        <v-pagination v-model="currentPage" :length="numPages"/>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -15,9 +39,23 @@ export default {
     ...mapState('events', {
       events: 'data',
     }),
+    eventsPerPage: () => 15,
+    numPages () {
+      return Math.ceil(this.events.length / this.eventsPerPage);
+    },
+    viewableEvents () {
+      const startIndex = (this.currentPage - 1) * this.eventsPerPage;
+      const endIndex = startIndex + this.eventsPerPage;
+      return this.events.filter((e, i) => i >= startIndex && i < endIndex);
+    },
   },
   methods: {
     ...mapActions('events', ['updateData']),
+  },
+  data () {
+    return {
+      currentPage: 1,
+    };
   },
   async mounted () {
     await this.updateData();
