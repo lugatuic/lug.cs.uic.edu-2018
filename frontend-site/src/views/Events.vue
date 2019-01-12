7d36fab4a63f0e6e02c1281be3874e4257430b4d
Joshua Castor <joshua.castor@joshuacastor.me>
2019-01-11 23:33:26 -0600

    Events Page: Full layout (#59)
    * Added initial logic for loading events on events page
    
    * Moved event card component to events folder
    
    * Added initial event listing
    
    * Added initial search card for events (sort only)
    
    * Added ability to filter by event name or description; increased events shown per page
    
    * reset page counter on events page on events input change
    
    * removed unnecessary style tags in event search card component

--- frontend-site/src/views/Events.vue
+++ frontend-site/src/views/Events.vue
@@ -1,3 +1,76 @@
 <template>
-  <h1>This is the events page</h1>
+  <v-container grid-list-lg>
+    <v-layout row>
+      <v-flex>
+        <search-card
+          :allEvents="allEvents"
+          v-model="events"/>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex class="text-xs-center" v-if="events.length === 0">
+        No events found.
+      </v-flex>
+      <template v-else>
+        <v-flex
+          v-for="(event, i) in viewableEvents"
+          :key="i"
+          xs12 md6>
+          <event-card :event="event"/>
+        </v-flex>
+      </template>
+    </v-layout>
+    <v-layout row>
+      <v-flex class="text-xs-center">
+        <v-pagination v-model="currentPage" :length="numPages"/>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+import EventCard from '@/components/Events/EventCard';
+import SearchCard from '@/components/Events/SearchCard';
+
+export default {
+  components: {
+    EventCard,
+    SearchCard,
+  },
+  computed: {
+    ...mapState(['useLightTheme']),
+    ...mapState('events', {
+      allEvents: 'data',
+    }),
+    eventsPerPage: () => 18,
+    numPages () {
+      return Math.ceil(this.events.length / this.eventsPerPage);
+    },
+    viewableEvents () {
+      const startIndex = (this.currentPage - 1) * this.eventsPerPage;
+      const endIndex = startIndex + this.eventsPerPage;
+      return this.events.filter((e, i) => i >= startIndex && i < endIndex);
+    },
+  },
+  methods: {
+    ...mapActions('events', ['updateData']),
+  },
+  data () {
+    return {
+      currentPage: 1,
+      events: [],
+    };
+  },
+  async mounted () {
+    await this.updateData();
+    this.events = this.allEvents.slice();
+  },
+  watch: {
+    events () {
+      // reset page count on new event listing
+      this.currentPage = 1;
+    },
+  },
+};
+</script>
